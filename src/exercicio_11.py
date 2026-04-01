366c5dd381cd0924df0455ae23b2685a12d075f9
Cecilia Pessoa <113480240+Cecihp@users.noreply.github.com>
2026-04-01 17:41:11 -0300

    finalizei lista 01

--- src/exercicio_11.py
+++ src/exercicio_11.py
@@ -1,17 +1,4 @@
-def modify_guest_list(
-    guests: list[str],
-    unavailable: str,
-    new_guest: str
-) -> list[str]:
-    """
-    Substitui um convidado indisponível por outro.
-
-    Args:
-        guests (list[str]): lista original
-        unavailable (str): convidado a ser removido
-        new_guest (str): novo convidado
-
-    Returns:
-        list[str]: lista atualizada
-    """
-    pass
+def modify_guest_list(guests, unavailable, new_guest):
+    index = guests.index(unavailable)
+    guests[index] = new_guest
+    return guests
